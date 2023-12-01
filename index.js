b4787c21570cacf7b8748caa23a095615e0b76ac
Nikhil Patil <75615119+nikhil37n@users.noreply.github.com>
2023-12-01 16:05:32 +0530

    Modal-popup in javascript with blur background

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggle() {
+    const blur = document.querySelector('.container');
+    blur.classList.toggle('active');
+    const popup = document.querySelector('#popup');
+    popup.classList.toggle('active');
+}
